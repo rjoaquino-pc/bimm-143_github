13f91740d85fd5ebaca16745c0b6c4cf18c6cd1e
Paige Contreras <rjoaquino@ucsd.edu>
2026-03-13 11:48:37 -0700

    add c4

---
+++ class04/class04.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Class 4 Intro to R"
+#' author: "Paige Contreras"
+#' date: "January 16, 2026"
+#' ---
+X <- 1:50
+plot(X)
+plot(X,sin(X))
+plot(X,sin(X), typ="o", col="pink", lwd=3, xlab="Is it Lunch Time Yet?")
